49c3f5684f38e625f53d8ae26a6bce5b459d625d
Asim <saffarrli14@gmail.com>
2024-03-02 00:50:29 +0400

    add first commit

---
+++ ChatGPTSwift/Application/ChatGPTSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTSwiftApp.swift
+//  ChatGPTSwift
+//
+//  Created by Asim Seferli on 02.03.24.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
